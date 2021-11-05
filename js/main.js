1b35e381e20de386f420ac62da985596894a9b24
Osama <osamamegahed2468@gmail.com>
2021-11-05 14:10:13 +0200

    d

---
+++ js/main.js
@@ -0,0 +1,16 @@
+menu=document.querySelector("#menu-bar")
+navbar=document.querySelector(".navbar")
+
+menu.addEventListener("click", () =>{
+    menu.classList.toggle("uil-times");
+    navbar.classList.toggle("active");
+})
+window.onscroll =() =>{
+    menu.classList.remove("uil-times");
+    navbar.classList.remove("active");
+}
+
+
+
+
+
