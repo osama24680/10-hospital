e2b0d4982f3829f35e61c3c461f4e86ee3f0e2d0
Osama <osamamegahed2468@gmail.com>
2022-07-05 15:52:14 +0200

    cvcx

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-menu=document.querySelector("#menu-bar")
-navbar=document.querySelector(".navbar")
+menu = document.querySelector("#menu-bar")
+navbar = document.querySelector(".navbar")
 
-menu.addEventListener("click", () =>{
+menu.addEventListener("click", () => {
     menu.classList.toggle("uil-times");
     navbar.classList.toggle("active");
 })
-window.onscroll =() =>{
+window.onscroll = () => {
     menu.classList.remove("uil-times");
     navbar.classList.remove("active");
 }
 
+let allLinks = document.getElementsByTagName("a")
 
-
-
+allLinks.addEventListener("click", function(e) {
+    e.preventDefault()
+})
 
