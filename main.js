04bd5e807d5c67b8b9bcd436019cfd42a5413034
Osama Megahed <osamamegahed2468@gmail.com>
2023-12-16 09:00:52 +0200

    new launch

---
+++ main.js
@@ -0,0 +1,8 @@
+let Details = document.querySelectorAll(".details");
+
+console.log(Array.from(Details));
+for (let i = 0; i < Details.length; i++) {
+  Details[i].addEventListener("click", function () {
+    window.open('https://thingspeak.com/channels/2382138', '_blank', 'width=1000,height=800');
+  });
+}
